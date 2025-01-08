78a77e929ea4a02a13dfefab2960088d4ded8985
zinine73 <zinine@naver.com>
2025-01-08 15:51:15 +0900

    프로그래머스 문제 풀이

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(solution(27, 19));
+    }
+
+    public static int solution(int num1, int num2) {
+        int answer = 0;
+        answer = num1 * num2;
+        return answer;
+    }
+}
