5b2015b3d21b71fb781ef7e48d08d2bace68a0f2
zinine73 <zinine@naver.com>
2025-01-09 15:49:59 +0900

    /// tooltip

--- Solution.cs
+++ Solution.cs
@@ -1,5 +1,11 @@
 class Solution
 {
+    /// <summary>
+    /// 두 수의 차
+    /// </summary>
+    /// <param name="num1">인자1</param>
+    /// <param name="num2">인자2</param>
+    /// <returns>반환값</returns>
     public int solution0109(int num1, int num2)
     {
         // 더 간단하게
@@ -15,6 +21,12 @@ class Solution
         //return anything;
     }
 
+    /// <summary>
+    /// 두 수의 곱
+    /// </summary>
+    /// <param name="num1"></param>
+    /// <param name="num2"></param>
+    /// <returns></returns>
     public int solution0108(int num1, int num2)
     {
         int answer = 0;
